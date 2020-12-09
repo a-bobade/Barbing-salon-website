e9bb2c6b54f4ef413843c61873cca59249c2f376
Emmanuel Abobade <emmanuelabobade@gmail.com>
2020-12-09 11:10:00 +0100

    Initial commit

--- barbing/settings.py
+++ barbing/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j)jkqho2p4t3bs@xfxoaskib+x-&1=#&m8i*@!&i44!drov*l+'
+SECRET_KEY = 'xxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
